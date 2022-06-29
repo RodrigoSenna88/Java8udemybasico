3e2f13e22f50af5a18eef52a1222b34fc00ba6d4
rodrigosennasantos <rodrigosennasantos@ibm.com>
2022-06-29 17:29:56 -0300

    Criando Repositório

---
+++ src/main/java/academy/learnprogramming/rest/TodoConfig.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package academy.learnprogramming.rest;
+
+
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author 016180631
+ */
+@ApplicationPath("api/v1")
+public class TodoConfig extends Application{
+    
+       
+}
